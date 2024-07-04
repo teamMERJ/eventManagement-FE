8995728c2af30980d223fe46f1306e90612a8ee8
yanquehlina <c.maameyankeh@gmail.com>
2024-07-04 13:33:57 +0000

    add event page updates

---
+++ src/assets/events.js
@@ -0,0 +1,28 @@
+import eventOneImg from "../assets/images/events-images/event-one.jpg";
+import eventTwoImg from "../assets/images/events-images/event-two.jpg";
+import eventThreeImg from "../assets/images/events-images/event-three.jpeg";
+import eventFourImg from "../assets/images/events-images/event-four.jpg";
+import eventFiveImg from "../assets/images/events-images/event-five.jpg";
+import eventSixImg from "../assets/images/events-images/event-six.jpg";
+import eventSevenImg from "../assets/images/events-images/event-seven.jpg";
+import eventEightImg from "../assets/images/events-images/event-eight.jpeg";
+import eventNineImg from "../assets/images/events-images/event-nine.jpg";
+import eventTenImg from "../assets/images/events-images/event-ten.webp";
+import eventElevenImg from "../assets/images/events-images/event-eleven.jpg";
+import eventTwelveImg from "../assets/images/events-images/event-twelve.jpg";
+
+
+export {
+  eventOneImg,
+  eventTwoImg,
+  eventThreeImg,
+  eventFourImg,
+  eventFiveImg,
+  eventSixImg,
+  eventSevenImg,
+  eventEightImg,
+  eventNineImg,
+  eventTenImg,
+  eventElevenImg,
+  eventTwelveImg,
+};
